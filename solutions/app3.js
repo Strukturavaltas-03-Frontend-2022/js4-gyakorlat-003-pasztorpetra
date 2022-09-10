615983f8b8327b49c25ebd6335b449662cc50336
Petra <pasztor.petra1998@gmail.com>
2022-09-10 23:53:50 +0200

    kész

--- solutions/app3.js
+++ solutions/app3.js
@@ -25,12 +25,19 @@
  * üres tömbbel
  */
  const getMovies = async (url = '') => {
-    
+    try {
+      const response = await fetch(url);
+      const result = await response.json();
+      return result;
+    } catch {
+      console.error(error);
+      return ([]);
+    }
 };
 
 /**
  * TODO: exportáld ki helyesen a getMovies függvényt!
  */
 export {
-    
+    getMovies
 }
